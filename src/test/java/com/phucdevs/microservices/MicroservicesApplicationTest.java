d3de2eec05025690a052f8d714c257d9df26e4eb
phuc.letan <phuc.letan@messagemedia.com>
2023-04-08 22:06:27 +0700

    Init project

---
+++ src/test/java/com/phucdevs/microservices/MicroservicesApplicationTest.java
@@ -0,0 +1,13 @@
+package com.phucdevs.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
