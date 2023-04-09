ce0343cbd825b5f0e62b896cf058c6e35b130688
phuc.letan <phuc.letan@messagemedia.com>
2023-04-09 22:20:44 +0700

    rm all .idea, update .gitignore

---
+++ lab-3/client-solution/src/main/java/com.phucdevs.microservices/LuckyWordController.java
@@ -0,0 +1,17 @@
+package com.phucdevs.microservices;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWordController {
+
+    @Value("${lucky-word}")
+    String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public String shouldLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
