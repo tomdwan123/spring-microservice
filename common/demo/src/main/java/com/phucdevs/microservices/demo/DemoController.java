3aea3f5aad6fe69d082c3f56a9ceecf996307a89
phuc.letan <phuc.letan@messagemedia.com>
2023-04-09 09:18:29 +0700

    Init common module

---
+++ common/demo/src/main/java/com/phucdevs/microservices/demo/DemoController.java
@@ -0,0 +1,19 @@
+package com.phucdevs.microservices.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @Autowired
+    TeamRepository repository;
+
+    @RequestMapping("/hi/{name}")
+    public @ResponseBody Team hello(@PathVariable String name) {
+        return repository.findByName(name);
+    }
+}
