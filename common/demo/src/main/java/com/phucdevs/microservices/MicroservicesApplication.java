c459a8ba719cc430634aae9eaed3412d66be7a11
phuc.letan <phuc.letan@messagemedia.com>
2023-04-09 09:23:31 +0700

    Init common module

---
+++ common/demo/src/main/java/com/phucdevs/microservices/MicroservicesApplication.java
@@ -0,0 +1,46 @@
+package com.phucdevs.microservices;
+
+import com.phucdevs.microservices.demo.Player;
+import com.phucdevs.microservices.demo.Team;
+import com.phucdevs.microservices.demo.TeamRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class MicroservicesApplication extends SpringBootServletInitializer {
+
+    /*
+     * Used with run by JAR
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesApplication.class, args);
+    }
+
+    /*
+     * Used with run by WAR
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MicroservicesApplication.class);
+    }
+
+    @PostConstruct
+    public void init() {
+
+        Set<Player> players = new HashSet<>();
+        players.add( new Player("Chicago", "US") );
+        players.add( new Player("Tokyo", "JP") );
+        Team team = new Team("Country", "Rich", "", players);
+        repository.save(team);
+    }
+
+    @Autowired
+    TeamRepository repository;
+}
---
+++ common/demo/src/main/java/com/phucdevs/microservices/demo/TeamRepository.java
@@ -0,0 +1,14 @@
+package com.phucdevs.microservices.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RestResource(path = "teams", rel = "teams")
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    List<Team> findAll();
+
+    Team findByName(String name);
+}
